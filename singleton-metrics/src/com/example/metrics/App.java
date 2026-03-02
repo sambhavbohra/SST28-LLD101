fd9a74f5a074d6817d44f3c75a3f81bb7deb13c1
sambhavbohra <sammybohra1008@gmail.com>
2026-03-02 19:19:04 +0530

    Error resolved

--- singleton-metrics/src/com/example/metrics/App.java
+++ singleton-metrics/src/com/example/metrics/App.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        String propsPath = "metrics.properties";
+        String propsPath = "src/metrics.properties";
 
         MetricsLoader loader = new MetricsLoader();
         MetricsRegistry loaded = loader.loadFromFile(propsPath);
